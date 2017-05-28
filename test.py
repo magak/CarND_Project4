8ca4bcf4681d9dc2793d2cab2daa552fcf1e22bc
magak <magak@rambler.ru>
2017-05-28 17:20:24 +0300

    First simple pipeline

---
+++ test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+img = mpimg.imread('test_images/straight_lines1.jpg')
+plt.imshow(img)
+plt.show()
+
+
+img = mpimg.imread('test_images/straight_lines2.jpg')
+plt.imshow(img)
+plt.show()
